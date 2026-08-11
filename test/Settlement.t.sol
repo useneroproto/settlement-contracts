c909f56da1bfda7ce8331c565df742d0b2c76edc
Verity Protocol <verityproto@users.noreply.github.com>
2026-08-11 18:39:20 +0300

    fix: correct import path to contracts/

--- test/Settlement.t.sol
+++ test/Settlement.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../src/Settlement.sol";
+import "../contracts/Settlement.sol";
 
 contract SettlementTest is Test {
     Settlement public settlement;
